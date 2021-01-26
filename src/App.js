57f66a7904ca85dba12c0bb639930cf9661bca32
girisimci <saitisik63@gmail.com>
2021-01-26 23:23:25 +0300

    Stiller değiştirildi navbar eklendi

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 
-import './App.css';
-
+import Home from './Home';
+import Navbar from "./Navbar";
 function App() {
 
-  const title= "Welcome To The New Blog";
-  const likes=5;
-  const person={age:25,name:"Mehmet Sait Işık",job:"Frontend / Mobile Developer"}
   return (
     <div className="App">
+      <Navbar/>
       <div className="content">
-       <h1>{title}</h1>
-       <p>Liked {likes} times</p>
-        <p>{person}</p>
+      <Home/>
       </div>
     </div>
   );
