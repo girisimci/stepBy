9eb5361ae8033714db14d227cd827a7059440277
girisimci <saitisik63@gmail.com>
2021-01-28 19:23:49 +0300

    devamı gelecek..

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import BlogList from "./BlogList";
 const Home = () => {
 
@@ -13,6 +13,9 @@ const Home = () => {
       setBlogs(newBlogs);
     }
 
+    useEffect(()=>{
+      console.log('selamlar');
+    })
     return (
         <div className="home">
           <BlogList blogs={blogs} title="Bütün bloglar" handleDelete={handleDelete}/>
