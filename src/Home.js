57c8c4767e142b19b29fede27d38ad87f65ba140
girisimci <saitisik63@gmail.com>
2021-01-28 16:56:35 +0300

    filtreleme silme özelliği

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,22 @@
+import { useState } from "react";
+import BlogList from "./BlogList";
 const Home = () => {
+
+    const [blogs,setBlogs]=useState([
+        {title:'My new website',body:'birinci lorems ...',author:'Sait',id:1},
+        {title:'Welcome Party',body:'ikinci lorem ...',author:'Mehmet',id:2},
+        {title:'Web Dev Top Tips',body:'üçüncü lorem ...',author:'Sait',id:3},
+    ]);
+
+    const handleDelete = (id)=>{
+      const newBlogs = blogs.filter(blog=>blog.id !== id);
+      setBlogs(newBlogs);
+    }
+
     return (
         <div className="home">
-            <h2>Home Page</h2>
+          <BlogList blogs={blogs} title="Bütün bloglar" handleDelete={handleDelete}/>
+         
         </div>
       );
 }
